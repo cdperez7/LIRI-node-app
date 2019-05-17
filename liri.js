1b003a02763b05dcd3fa1c0e0dfcdac678d8017e
Connor Perez <connorperez@gmail.com>
2019-05-17 08:50:27 -0500

    keys and set up ready

--- liri.js
+++ liri.js
@@ -1,4 +1,12 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
+var fs = require("fs");
+var request = require("request");
+var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
+
+var userOption = process.argv[2];
+var inputParameter = process.argv[3];
+
+UserInputs(userOption, inputParameter);
